9e970798146134a651895334f2a34efc9b1f5816
nidebao <3145129171@qq.com>
2026-05-20 06:14:06 +0000

    1.3

--- src/main/java/com/daniel/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/daniel/service/impl/CategoryServiceImpl.java
@@ -1,10 +1,14 @@
 package com.daniel.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import com.daniel.dao.CategoryDAO;
 import com.daniel.pojo.Category;
 import com.daniel.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import java.util.HashMap;
 import java.util.List;
@@ -16,9 +20,24 @@ public class CategoryServiceImpl implements CategoryService{
     @Autowired
     CategoryDAO categoryDAO;
 
+    @Autowired
+    JedisPool jedisPool;
+
+    private static final String CATEGORY_LIST_KEY = "categories:list";
+
     @Override
     public List<Category> list() {
-        return categoryDAO.list();
+        try (Jedis jedis = jedisPool.getResource()) {
+            String json = jedis.get(CATEGORY_LIST_KEY);
+            if (json != null) {
+                return JSON.parseObject(json, new TypeReference<List<Category>>() {});
+            }
+            List<Category> categories = categoryDAO.list();
+            jedis.setex(CATEGORY_LIST_KEY, 3600, JSON.toJSONString(categories));
+            return categories;
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     @Override
